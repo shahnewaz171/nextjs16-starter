eb47617e77568fc950c947ed1b1e9e15b7644620
shahnewaz171 <shahnewaz601@gmail.com>
2025-12-09 00:50:42 +0600

    refactor: updated config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  cacheComponents: true
+  reactCompiler: true
+  // cacheComponents: true
 };
 
 export default nextConfig;
